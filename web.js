c2e3684191a92ddf83b2a241c403e61029d69bef
Brian Dorton <dorton@gmail.com>
2015-04-17 23:35:08 -0500

    modifies web.js

--- web.js
+++ web.js
@@ -1,9 +1,5 @@
-var express = require("express");
+var express = require('express');
+var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.logger());
-app.use("/", express.static(__dirname));
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+app.use(express.static(__dirname + ‘/public’));
+app.listen(port);
